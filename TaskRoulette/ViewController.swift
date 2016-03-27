81292f58a0b52541f0e854d3b2bf0a6d45fe928b
hikariyamasaki <hyamasaki0101@gmail.com>
2016-03-27 17:08:29 +0900

    Initial Commit

---
+++ TaskRoulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskRoulette
+//
+//  Created by 山崎ひかり on 2016/03/27.
+//  Copyright © 2016年 hikariyamasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
